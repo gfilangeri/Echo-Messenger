5fe64cfe21df9db5fef6ab2182b6599a52372603
gfilangeri <gfilange@calpoly.edu>
2019-04-28 14:12:05 -0700

    initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="app">
+    <div class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <router-link class="navbar-item is-tab" to="/" exact-active-class="is-active">Home</router-link>
+          <router-link class="navbar-item is-tab" to="/about" exact-active-class="is-active">About</router-link>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary" v-if="!isLoggedIn" v-on:click="showSignupModal()">
+                <strong>Sign up</strong>
+              </a>
+              <router-link
+                class="button is-text"
+                v-if="isLoggedIn"
+                to="/my-profile"
+                exact-active-class="is-active"
+              >My Profile</router-link>
+              <a class="button is-light" v-if="!isLoggedIn" v-on:click="showLoginModal()">Log in</a>
+              <a class="button is-light" v-if="isLoggedIn" v-on:click="logout">Log out</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view class="container"/>
+    <Signup
+      v-bind:is-showing="showSignup"
+      v-on:success="successSignup()"
+      v-on:cancel="cancelSignup()"
+    />
+    <Login v-bind:is-showing="showLogin" v-on:success="successLogin()" v-on:cancel="cancelLogin()"/>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Signup from "@/components/Signup.vue";
+import Login from "@/components/Login.vue";
+import { APIConfig } from "@/utils/api.utils";
+
+@Component({
+  components: {
+    Signup,
+    Login
+  }
+})
+export default class App extends Vue {
+  public showSignup: boolean = false;
+  public showLogin: boolean = false;
+
+  showSignupModal() {
+    this.showSignup = true;
+  }
+
+  successSignup() {
+    this.showSignup = false;
+  }
+
+  cancelSignup() {
+    this.showSignup = false;
+  }
+
+  showLoginModal() {
+    this.showLogin = true;
+  }
+
+  successLogin() {
+    this.showLogin = false;
+  }
+
+  cancelLogin() {
+    this.showLogin = false;
+  }
+
+  get isLoggedIn(): boolean {
+    return !!this.$store.state.user;
+  }
+
+  logout() {
+    axios
+      .post(APIConfig.buildUrl("/logout"), null, {
+        headers: { token: this.$store.state.userToken }
+      })
+      .then(() => {
+        this.$store.commit("logout");
+        this.$router.push({ name: "home" });
+      });
+  }
+}
+</script>
+
+
+<style lang="scss">
+@import "~bulma/css/bulma.css";
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
