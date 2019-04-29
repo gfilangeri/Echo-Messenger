105646fdf6a3cb7d25b0b93b3866ffd3f3023e29
gfilangeri <gfilange@calpoly.edu>
2019-04-28 17:43:34 -0700

    yay

--- client/src/App.vue
+++ client/src/App.vue
@@ -20,27 +20,36 @@
         </md-toolbar>
 
         <md-list>
-          <md-list-item>
-            <router-link to="/chat" tag="li" class="nav_button">
-                <md-icon>chat_bubble</md-icon>
-            </router-link>
+           <router-link to="/chat" tag="li">
+          <md-list-item @click="changeActive(0)"> 
+              <md-icon
+                v-if="chatActive"
+                class="nav_button_active"
+              >chat_bubble</md-icon>
+              <md-icon
+                v-if="!chatActive"
+                class="nav_button"
+              >chat_bubble</md-icon>
+            
             <span class="md-list-item-text">Messages</span>
           </md-list-item>
+          </router-link>
 
-          <md-list-item>
-            <router-link to="/contacts" tag="li">
-                <md-icon class="nav_button">perm_contact_calendar</md-icon>
-            </router-link>
+<router-link to="/contacts" tag="li">
+          <md-list-item  @click="changeActive(1)">
+              <md-icon class="nav_button">perm_contact_calendar</md-icon>
             <span class="md-list-item-text">Contacts</span>
           </md-list-item>
+          </router-link>
 
-          <md-list-item>
-            <router-link to="/my-profile" tag="li">
-                <md-icon class="nav_button">account_circle</md-icon>
-            </router-link>
+<router-link to="/my-profile" tag="li">
+          <md-list-item  @click="changeActive(2)">
+              <md-icon class="nav_button">account_circle</md-icon>
             <span class="md-list-item-text">Profile</span>
           </md-list-item>
+          </router-link>
         </md-list>
+        
       </md-app-drawer>
 
       <md-app-content>
@@ -76,15 +85,68 @@ Vue.component("menu-icon", MenuIcon);
 export default {
   name: "PersistentMini",
   data: () => ({
-    menuVisible: false
+    menuVisible: false,
+    chatActive: false,
+    contactsActive: false,
+    profileActive: false
   }),
   methods: {
     toggleMenu() {
       this.menuVisible = !this.menuVisible;
-    }
+    },
+          changeActive(num) {
+      if (num == 0) {
+        this.chatActive = true;
+        this.contactsActive = false;
+        this.profileActive = false;
+      }
+      if (num == 1) {
+        this.chatActive = false;
+        this.contactsActive = true;
+        this.profileActive = false;
+      }
+      if (num == 2) {
+        this.chatActive = false;
+        this.contactsActive = false;
+        this.profileActive = true;
+      }
+      console.log(this.chatActive);
+      console.log(this.contactsActive);
+      console.log(this.profileActive);
   }
+  },
 };
+// export default class App extends Vue {
+//   menuVisible: Boolean = false;
+//   chatActive: Boolean = false;
+//   contactsActive: Boolean = false;
+//   profileActive: Boolean = false;
 
+//   toggleMenu() {
+//     this.menuVisible = !this.menuVisible;
+//   }
+
+//   changeActive(num: number) {
+//     if (num == 0) {
+//       this.chatActive = true;
+//       this.contactsActive = false;
+//       this.profileActive = false;
+//     }
+//     if (num == 1) {
+//       this.chatActive = false;
+//       this.contactsActive = true;
+//       this.profileActive = false;
+//     }
+//     if (num == 2) {
+//       this.chatActive = false;
+//       this.contactsActive = false;
+//       this.profileActive = true;
+//     }
+//     console.log(this.chatActive);
+//     console.log(this.contactsActive);
+//     console.log(this.profileActive);
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>
@@ -96,7 +158,7 @@ export default {
 .nav_button {
   color: black;
 }
-.nav_button:active {
+.nav_button_active {
   color: green;
 }
 .nav_button:hover {
